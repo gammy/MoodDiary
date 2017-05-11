ad34c4d35e6952190c7d8e3e3ec3c792df497a5a
gammy <gammy@noreply.github.com>
2017-05-11 12:38:08 +0200

    Add cheap paypal button. This is alright since I'm not on the playstore :P

--- app/src/main/java/nu/ere/mooddiary/AboutActivity.java
+++ app/src/main/java/nu/ere/mooddiary/AboutActivity.java
@@ -1,10 +1,14 @@
 package nu.ere.mooddiary;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends ThemedActivity {
     @Override
@@ -14,5 +18,15 @@ public class AboutActivity extends ThemedActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setContentView(R.layout.about_main);
+        Button donateButton = (Button) findViewById(R.id.donateButton);
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = getResources().getString(R.string.paypal_action_link);
+                Intent browserIntent =
+                        new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
