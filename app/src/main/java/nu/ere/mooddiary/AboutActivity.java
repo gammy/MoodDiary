a7b77d5f7c94c0117ed577d570955e3e79318e27
gammy <gammy@noreply.github.com>
2017-05-16 14:50:04 +0200

    Add stupid version info to the stupid about section. If this commit is a repeat, then it's AS's fault.

--- app/src/main/java/nu/ere/mooddiary/AboutActivity.java
+++ app/src/main/java/nu/ere/mooddiary/AboutActivity.java
@@ -1,5 +1,6 @@
 package nu.ere.mooddiary;
 
+import nu.ere.mooddiary.BuildConfig;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends ThemedActivity {
     private static final String LOG_PREFIX = "AboutActivity";
@@ -17,10 +19,22 @@ public class AboutActivity extends ThemedActivity {
         Log.d(LOG_PREFIX, "Create");
         super.onCreate(savedInstanceState);
         Toolbar toolbar = (Toolbar) findViewById(R.id.aboutToolbar);
+        // FIXME if I try to use the toolbar, app crashes
         setSupportActionBar(toolbar);
-        // toolbar.setTitle("Mood Diary"); // FIXME
+
         setContentView(R.layout.content_about);
+
+        int versionCode = BuildConfig.VERSION_CODE;
+        String versionName = BuildConfig.VERSION_NAME;
+
+        String stupid = getString(R.string.text_about);
+        stupid = "Mood Diary v" + versionName +
+                " (" + Integer.toString(versionCode) +") " + stupid;
+        TextView view = (TextView) findViewById(R.id.aboutText);
+        view.setText(stupid);
+
         Button donateButton = (Button) findViewById(R.id.donateButton);
+
         donateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
